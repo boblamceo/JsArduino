47f52680a223b846b6d2cb71f0c87931e33fc76a
boblamceo <boblam.ceo@gmail.com>
2024-08-21 19:30:28 +0800

    working again again

--- strobe.js
+++ strobe.js
@@ -8,7 +8,9 @@ board.on("ready", () => {
     let stream = fs.createWriteStream(`data/sample_punch_0.txt`, {
         flags: "a",
     });
+    console.log("hi");
     imu.on("data", function () {
+        console.log("hi");
         let data = `${this.accelerometer.x} ${this.accelerometer.y} ${this.accelerometer.z} ${this.accelerometer.pitch} ${this.accelerometer.roll} ${this.accelerometer.acceleration} ${this.accelerometer.inclination} ${this.accelerometer.orientation} ${this.gyro.x} ${this.gyro.y} ${this.gyro.z}`;
 
         stream.write(`${data}\r\n`);
